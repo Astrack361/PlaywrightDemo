33c8c5af9e0b6909eb08098ad4587fc994200618
Guillermo <astrack201@gmail.com>
2025-01-20 21:53:45 -0600

    before tag added to my test cases

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,14 +1,24 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://playwright.dev/');
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+
+
+test('Homepage has Playwright in title', async ({ page }) => {
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+  
+});
+
+test('Main page and get started button', async ({ page }) => {
+  //Create locator into a variable
+  const getStarted = page.locator('text=Get Started');
+
+  //Validate if variable is equal than the expected value
+  await expect(getStarted).toHaveAttribute('href', '/docs/intro');
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
